84d8aa351432a21e1ced72739b1a14e2200db91e
Nordin <nordin.aljaberri@edu-kw1c.nl>
2020-02-10 15:24:35 +0100

    opdracht_2-2.php af

--- hoofdstuk2/opdracht_2-2.php
+++ hoofdstuk2/opdracht_2-2.php
@@ -6,8 +6,34 @@
  * File: Opdracht 2.2.php
  */
 
-$text1 = "Hallo";
+?>
 
-echo $text1;
+<body>
+<header>
+    <h1> <a href="../index.php">Terug</a>|PHP-opdracht 2.2</h1>
+</header>
+
+<?php
+
+$text1 = "Hallo";
+$text2 = "een makkelijke taal ";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat is ";
+$text5 = "PHP ";
+$text6 = "Nooit gedacht dat ";
+$text7 = "De installatie is best ingewikkeld";
+$text8 = "Fijn toch ";
+$text9 = "? ";
+$text10 = ". ";
+$text11 = ", ";
+$text12 = "<br> ";
+$text13 = " is";
+$text14 = "Vind je niet ";
+
+echo "<p>".$text1.$text11.$text4.$text5.$text3.$text10.$text12.$text7.$text10.$text8.$text9.$text12.$text6.$text5.$text3.$text13."</p>";
+?>
+<?php
+echo "<p>".$text1.$text12.$text8.$text5.$text3.$text13.$text10.$text12.$text7.$text10.$text14.$text9."</p>";
 
 ?>
+</body>
